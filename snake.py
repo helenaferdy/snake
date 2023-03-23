c485d739cad263af6bc6572efcbef1f0fbf95c36
Sena <36813961+helenaferdy@users.noreply.github.com>
2023-03-23 13:59:02 +0700

    "game over text"

--- snake.py
+++ snake.py
@@ -96,8 +96,9 @@ class Snake:
 
     def end_game(self):
         eg = ScoreBoard()
-        eg.goto(-50, 0)
-        eg.write(f"GAME OVER", font=("Arial", 20, "normal"))
+        eg.goto(-100, 0)
+        eg.color("red")
+        eg.write(f"GAME OVER", font=("Arial", 40, "normal"))
         self.logz.info(f"game over at {self.head.pos()} with score {self.foodscore}")
 
     def move_up(self):
